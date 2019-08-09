91fa3396cfc24bbc5ddb9fc2ba153c30fd5b917b
Benjamin Fox <foxbenjaminfox@gmail.com>
2019-08-09 12:24:25 -0500

    Change configuration format to YAML

--- src/config.rs
+++ src/config.rs
@@ -11,11 +11,11 @@ pub struct Config {
 impl Config {
     pub fn get() -> Fallible<Config> {
         let xdg = xdg::BaseDirectories::with_prefix("taskratchet")?;
-        let config_path = xdg.place_config_file("config.json")?;
+        let config_path = xdg.place_config_file("config.yaml")?;
         let config_file = File::open(&config_path)
             .map_err(|_| format_err!("Config file {:?} does not exist.", config_path))?;
 
-        let config: Config = serde_json::from_reader(config_file).map_err(|_| {
+        let config: Config = serde_yaml::from_reader(config_file).map_err(|_| {
             format_err!(
                 "Config file {:?} could not be read; maybe a syntax error?",
                 config_path
